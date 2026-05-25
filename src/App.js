6345f22150177be1583dac8005b7a85869d5c298
prashik4629 <meshrampa@rknec.edu>
2026-05-25 11:38:35 +0530

    fix: add appointments page and regenerate lock

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Patients from './pages/Patients';
 import Doctors from './pages/Doctors';
 import Dashboard from './pages/Dashboard';
+import Appointments from './pages/Appointments';
 import './index.css';
 
 function App() {
@@ -14,13 +15,15 @@ function App() {
           <Link to="/">Dashboard</Link>
           <Link to="/patients">Patients</Link>
           <Link to="/doctors">Doctors</Link>
+          <Link to="/appointments">Appointments</Link>
         </nav>
 
         <div className="container">
           <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/patients" element={<Patients />} />
-            <Route path="/doctors" element={<Doctors />} />
+            <Route path="/"            element={<Dashboard />} />
+            <Route path="/patients"    element={<Patients />} />
+            <Route path="/doctors"     element={<Doctors />} />
+            <Route path="/appointments" element={<Appointments />} />
           </Routes>
         </div>
       </div>
